f6282b53909ad8674aefbb36a523ee89f7623cb3
DevCBasic <thanhit027@gmail.com>
2026-04-05 10:31:37 +0700

    remove

--- python/nonogram_detector/rectangle_functions.py
+++
@@ -1,7 +0,0 @@
-import cv2
-
-
-def rectangle_approx_poly(contour):
-    epsilon = 0.01 * cv2.arcLength(contour, True)
-    approx = cv2.approxPolyDP(contour, epsilon, True)
-    return approx if len(approx) == 4 else None
